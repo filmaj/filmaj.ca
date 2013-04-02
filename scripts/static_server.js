6ea729a0068fd73bc3c57d0c57dd2124b0ee724a
Fil Maj <maj.fil@gmail.com>
2013-04-02 15:40:00 -0400

    added presentations from mobify and philly

--- scripts/static_server.js
+++ scripts/static_server.js
@@ -1,15 +1,22 @@
 var http = require("http"),
+    https = require("https"),
     url = require("url"),
     path = require("path"),
     fs = require("fs")
-    port = process.argv[2] || 8888;
+    port = process.argv[2] || 8888,
+    isHTTPS = process.argv[3] ? true : false;
 
-http.createServer(function(request, response) {
+var options = isHTTPS ? {
+    key:fs.readFileSync('key.pem'),
+    cert:fs.readFileSync('cert.pem')
+} : {};
 
+function handler(request, response) {
   var uri = url.parse(request.url).pathname
     , filename = path.join(process.cwd(), uri);
   
   fs.exists(filename, function(exists) {
+      console.log(filename, exists);
     if(!exists) {
       response.writeHead(404, {"Content-Type": "text/plain"});
       response.write("404 Not Found\n");
@@ -17,7 +24,7 @@ http.createServer(function(request, response) {
       return;
     }
 
-	if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+	if (fs.statSync(filename).isDirectory()) filename += 'index.html';
 
     fs.readFile(filename, "binary", function(err, file) {
       if(err) {        
@@ -32,6 +39,14 @@ http.createServer(function(request, response) {
       response.end();
     });
   });
-}).listen(parseInt(port, 10));
+}
 
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+var server;
+if (isHTTPS) {
+    server = https.createServer(options, handler);
+} else {
+    server = http.createServer(handler);
+}
+server.listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http" + (isHTTPS ? 's':'') + "://localhost:" + port + "/\nCTRL + C to shutdown");
